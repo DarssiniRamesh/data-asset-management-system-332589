af060c919e82fe37fa1fa8dc1f9aed2061d4e3d8
Kavia AI <production@kavia.ai>
2026-03-16 06:18:12 +0000

    Checkpoint 10 - cm9ea7fa7f

--- data_asset_backend.Tests/obj/Release/net8.0/DataAssetBackend.Tests.AssemblyInfo.cs
+++ data_asset_backend.Tests/obj/Release/net8.0/DataAssetBackend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAssetBackend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5b9bd35a2da15f35d791d499f312502ac7416c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c5c434ff1d010dfd2359a3dce195a158f966f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAssetBackend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAssetBackend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
